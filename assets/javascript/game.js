dbcb1e767a7c99f0e2da34688440989d716cefc8
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-02-17 20:53:06 -0700

    Listed variables from HTML in JAVA program

---
+++ assets/javascript/game.js
@@ -0,0 +1,7 @@
+//listing variables... Matching with HTML ID for variables
+let wins = document.getElementById("wins")
+let losses = document.getElementById("losses")
+let myGuess = document.getElementById("your-guess")
+let cGuess = document.getElementById("computer-guess")
+let guesses = document.getElementById("guesses")
+let choices = document.getElementById("choices")
