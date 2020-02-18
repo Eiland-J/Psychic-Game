57b1152ae38ae96c3e5a6309835117f23febd8a2
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-02-17 21:36:08 -0700

    Finished JAVA added notes for better understanding on what each line does

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ let cGuess = document.getElementById("computer-guess")
 let guesses = document.getElementById("guesses")
 let choices = document.getElementById("choices")
 
-// array of the alphabet 27 elements
+// array of the alphabet 26 elements
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
 "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -17,3 +17,54 @@ var yourChoices = [];
 let guessesNum = 10;
 let winsNum = 0;
 let lossNum = 0;
+
+
+//function for when user chooses a letter
+document.onkeyup = (function (event) {
+
+    //taking yourChoices empty array and adding user input
+    yourChoices.push(event.key);
+    //connecting and wrtting with "innerHTML" users input/key pressed
+    choices.innerHTML = yourChoices;
+  
+    //Conditions on if a key is pressed 
+    if (event.keyCode ) {
+      var mGuess = event.key;
+      //Converts all letters to lowercase when pushed
+      myGuess.innerHTML = mGuess.toLowerCase();
+  
+       //checking if user key is a valid input (must be a letter)
+       var isValid = alphabet.includes(mGuess);
+  
+      //chooses random number 0-26 or "A-Z"
+      let randomNum = Math.floor(Math.random() * 26)
+  
+      //computer chooses whatever number is picked from randomNum array
+      var comGuess = alphabet[randomNum]
+      cGuess.innerHTML = comGuess
+  
+      //if user chooses correctly, win counter ++
+      if (mGuess === comGuess) {
+        winsNum++
+        wins.innerHTML = winsNum
+      }
+      //if user key is invalid gives an alert
+      else if( isValid === false) {
+        alert("Please choose a letter!!! \n Invalid Entry!");
+      }
+      else {
+         //if user chooses incorrectly, losses go up ++
+        lossNum ++
+        losses.innerHTML = lossNum
+        //amount of guesses go down by one
+        guessesNum --
+        guesses.innerHTML = guessesNum;
+      }
+    }
+  
+    //Tells user the game is over suggets refreshing
+    if (guessesNum == 0) {
+    alert("GAME OVER!!! \n Refresh Page to start over!")
+    } 
+  })
+  
