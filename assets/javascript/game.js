4e550fcfe6efd1f700a315151d52e5770f0e5030
Joewan Eiland <Joewan.Eiland@gmail.com>
2020-02-17 20:56:58 -0700

    Added array which contains the 27 letters of the alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,3 +5,7 @@ let myGuess = document.getElementById("your-guess")
 let cGuess = document.getElementById("computer-guess")
 let guesses = document.getElementById("guesses")
 let choices = document.getElementById("choices")
+
+// array of the alphabet 27 elements
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
